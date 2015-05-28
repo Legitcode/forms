acd24aeb6d03a27972c0c6e5c4fb5953bc3484df
Zach <zackify@gmail.com>
2015-05-28 03:52:40 -0700

    fixed link to components

--- forms.js
+++ forms.js
@@ -1,7 +1,7 @@
-import Form from '../lib/form';
-import Schema from '../lib/schema';
-import List from '../lib/list';
-import Property from '../lib/property';
+import Form from './lib/form';
+import Schema from './lib/schema';
+import List from './lib/list';
+import Property from './lib/property';
 
 export {
   Form,
