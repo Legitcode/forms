ecb26631cd9a43f0a87c2c46fb5a3b03510b35f2
Zach <zackify@gmail.com>
2015-05-28 03:51:49 -0700

    added exports for each component

---
+++ forms.js
@@ -0,0 +1,11 @@
+import Form from '../lib/form';
+import Schema from '../lib/schema';
+import List from '../lib/list';
+import Property from '../lib/property';
+
+export {
+  Form,
+  Schema,
+  Property,
+  List
+}
