be800029973d33da08c4ebf8c2aef3537f2f49d2
dphaener <dphaener@gmail.com>
2015-06-04 16:28:08 -0700

    Added password to inputs

---
+++ src/controls/password_input.jsx
@@ -0,0 +1,24 @@
+"use strict";
+
+import BaseInput from './base_input';
+import React from 'react';
+import _ from 'underscore';
+
+export default class NumberInput extends BaseInput {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <input
+        className={this.props.classes}
+        type="password"
+        ref={this.props.name}
+        name={this.props.name}
+        onChange={this.debouncedChange}
+        onBlur={this.onBlur}
+        defaultValue={this.props.defaultValue} />
+    )
+  }
+}
